6e65899d7e60cefac28479f4ec3eca7e5723dfc1
Mihai H <59961959+automatica-cluj@users.noreply.github.com>
2022-02-19 19:43:51 +0200

    Exemplu 1

---
+++ curs1sm/src/main/java/ro/utcluj/isp/curs1sm/Test.java
@@ -0,0 +1,8 @@
+
+package ro.utcluj.isp.curs1sm;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HELLO!");
+    }
+}
---
+++ curs1sm/src/main/java/ro/utcluj/isp/curs1sm/exemplu2/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.utcluj.isp.curs1sm.exemplu2;
+
+/**
+ *
+ * @author mihai.hulea
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HELLO2");
+    }
+}
